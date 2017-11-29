13f4bc0dcda72f7ad3cbd7d71e8fa66d1cf1358b
Tomasz Pleśniak <31666201+Tomke997@users.noreply.github.com>
2017-11-29 19:54:08 +0100

    lets try

---
+++ Tunes/src/tunes/bll/BllManager.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tunes.bll;
+
+import java.sql.SQLException;
+import java.util.List;
+import tunes.be.Songs;
+import tunes.dal.ConnectionManager;
+
+/**
+ *
+ * @author Pepe15224
+ */
+public class BllManager {
+  ConnectionManager cm = new ConnectionManager();
+    public List<Songs> getAllSongs() throws SQLException
+    {
+      return cm.getAllSongs();
+    }
+    public void addSong(Songs song)
+    {
+    cm.addSong(song);
+    }
+}
